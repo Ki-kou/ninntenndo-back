47ca451b513814dfd8b813957fd371358095fecd
kouhei <aoikuma1234@gmail.com>
2021-12-12 19:37:27 +0900

    test

--- app.py
+++ app.py
@@ -8,9 +8,31 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-from data import soft_name_list
+
 
 import os
+import time
+from selenium import webdriver
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Chrome('/usr/local/bin/chromedriver',options=options)
+driver.get('https://www.nintendo.co.jp/software/campaign/index.html')
+
+soft_name_list = []
+while True:
+    for element in driver.find_elements_by_class_name("nc3-c-softCard__name"):
+        soft_name_list.append(element.text)
+    try:
+        next_page = driver.find_element_by_class_name("nc3-c-pagination__next")
+        next_page.click()
+        time.sleep(2)
+    except ElementNotInteractableException:
+        driver.quit()
+        break
+
 app = Flask(__name__)
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ['CHANNEL_ACCESS_TOKEN']
 YOUR_CHANNEL_SECRET = os.environ['CHANNEL_SECRET']
