4b5f35623d659cceb3d1aa60396c88b9d096c3a5
kouhei <aoikuma1234@gmail.com>
2021-12-17 11:08:06 +0900

    test

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 results = cursor.fetchall()
-print("results" = results)
+print(results)
 
 @app.route("/callback", methods=['POST'])
 def callback():
